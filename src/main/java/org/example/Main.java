9f5e2c6cea7a3a88b1bd65f7454ede6e1f23b832
Rijaia <zaya_89@mail.ru>
2023-05-02 13:01:42 +0300

    Homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Homework Set and Map");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1() {
         System.out.println("Task 1");
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         for (Integer num : nums) {
@@ -11,7 +18,11 @@ public class Main {
                 System.out.print(num + " ");
             }
         }
+        System.out.println();
+    }
+    public static void task2() {
         System.out.println("Task 2");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         List<Integer> task2 = new ArrayList<>();
         for (Integer num : nums) {
             if (num % 2 == 0) {
@@ -21,5 +32,31 @@ public class Main {
         Collections.sort(task2);
         Set<Integer> end = new HashSet<>(task2);
         System.out.print(end);
+        System.out.println();
+    }
+    public static void task3() {
+        System.out.println("Task 3");
+        List<String> strings = new ArrayList<>(List.of("один", "один", "два","два", "два",
+                "три", "три", "три"));
+        Set<String> words = new HashSet<>(strings);
+        System.out.print(words);
+        System.out.println();
+    }
+    public static void task4() {
+        System.out.println("Task 4");
+        List<String> strings = new ArrayList<>(List.of("один", "один", "два","два", "два",
+                "три", "три", "три"));
+        Map<String, Integer> words = new HashMap<>();
+        for (String string : strings) {
+            if (words.containsKey(string)) {
+                words.put(string, words.get(string) + 1);
+            } else {
+                words.put(string, 1);
+            }
+        }
+        for (String key : words.keySet()) {
+            System.out.println("Слово "+ key+" встречается "+ words.get(key)+" раза");
+            }
+
     }
 }
